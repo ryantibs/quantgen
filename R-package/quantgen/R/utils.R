ef7185db1373341c45e08e95eeb13bcd9d4b994c
Ryan Tibshirani <ryantibs@gmail.com>
2020-12-02 14:57:20 -0500

    Update logit_pad() offset

--- R-package/quantgen/R/utils.R
+++ R-package/quantgen/R/utils.R
@@ -98,13 +98,13 @@ exp_pad = function(a=1, b=1) return(function(x) (exp(x)-b)/a)
 #' Returns functions that map \eqn{x \mapsto \log(\frac{ax+b}{1-ax+b})} and
 #' \eqn{x \mapsto \frac{\exp(x)(1+b)-b}{a(1+\exp(x))}}. (These are inverses.)
 
-logit_pad = function(a=1, b=1) return(function(x) log((a*x+b) / (1-a*x+b)))
+logit_pad = function(a=1, b=0.01) return(function(x) log((a*x+b) / (1-a*x+b))) 
 
 #' @rdname logit_pad
 #' @export
 
-sigmd_pad = function(a=1, b=1) return(function(x) (exp(x) * (1+b)-b) /
-                                                  ((1+exp(x)) * a))
+sigmd_pad = function(a=1, b=0.011) return(function(x) (exp(x) * (1+b)-b) /
+                                                      ((1+exp(x)) * a))
 #' Convenience function for uniform jitter
 #'
 #' Function to generate random draws from \eqn{\mathrm{Unif}[a,b]}.
